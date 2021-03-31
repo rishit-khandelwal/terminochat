a754f3bca0b8ef73050cb47e657dbd71a1ffcb5a
saptarshibasu15 <123saptarshi.basu@gmail.com>
2021-03-31 16:36:47 +0530

    init

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 import threading
 import socket
 
-host="127.0.0.1"
-port=6969
+host="terminochat.saptarshibasu.repl.co"
+port=80
 
 server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind((host,port))
