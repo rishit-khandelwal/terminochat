c54ca2af1c63a7df6df515e0cd136c50fb8b5caf
saptarshibasu15 <123saptarshi.basu@gmail.com>
2021-03-31 14:36:25 +0530

    init

---
+++ client.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+
+nickname=input("Choose a nickname: ")
+
+client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(("127.0.0.1",6969))
+
+def receive():
+    while True:
+        try:
+            msg=client.recv(1024).decode("ascii")
+            if msg=="NICK":
+                client.send(nickname.encode("ascii"))
+            else:
+                print(msg)
+        except:
+            print("Brr an error occured...")
+            client.close()
+            break
+
+def write():
+    while True:
+        msg=f"\033[92m{nickname}\033[00m: {input('')}"
+        client.send(msg.encode("ascii"))
+
+receive_thread=threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread=threading.Thread(target=write)
+write_thread.start()
